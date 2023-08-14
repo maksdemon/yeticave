36e52159080e42e998a1244eced6444498ead937
makhome <qwerty7732t@ya.ru>
2023-08-14 23:44:13 +0300

    sql23

--- queries.sql
+++ queries.sql
@@ -1,2 +1,19 @@
+
+
+
 INSERT INTO users SET  user_email = 'vasya@mail.ru',  user_password = 'secret';
 INSERT INTO users SET  user_email = 'vlad@mail.ru',  user_password = 'secretvlad';
+
+
+INSERT INTO
+    goods(title, price)
+VALUES
+    ("Вилка", 25),
+    ("Столовая ложка", 35),
+    ("Чайная ложка", 30)
+
+INSERT INTO
+    users (user_email, user_name,user_password)
+VALUES
+    ('vlad@mail.ru', 'vlad','secretvlad'),
+    ('vlad5@ma.ru', 'vlad7','secretvhhlad');
